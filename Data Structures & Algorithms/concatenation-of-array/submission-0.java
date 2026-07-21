ae4876a58da67e106a7cdf76167eca66076ddd8b
Md Nashit Perwaiz Shahi <146984755+nashitshahi17@users.noreply.github.com>
2026-07-21 14:13:55 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[2*n];
+        int k=0;;
+        for(int i=0;i<n;i++){
+            res[k++] = nums[i];
+        }
+        for(int i=0;i<n;i++){
+            res[k+i] = nums[i];
+        }
+        return res;
+    }
+}
